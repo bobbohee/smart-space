44469f6ff3760e24d90c600d2ff1be09cddac33d
park-bohee <41457508+park-bohee@users.noreply.github.com>
2019-01-27 22:31:12 +0900

    init project.html 2018

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ $(function() {
       $(this).toggleClass('active-' + (index + 1));
     })
   });
+
+  /* project */
+  var pjList =  $('#project #sec2 ul li');
+  for (let i = 0; i < pjList.length; i++) {
+    var pjImg = $(pjList[i]).children('a').children('img').height();
+
+    $(pjList[i]).height(pjImg);
+    $(pjList[i]).children('a').height(pjImg);
+    $(pjList[i]).children('a').children('.pjBg').height(pjImg);
+  }
 });
 
 /* 햄버거 메뉴 사이드 */
