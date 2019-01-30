b407dc888ccc346f9bc1c1720feb6a52c4b2f79f
park-bohee <41457508+park-bohee@users.noreply.github.com>
2019-01-31 01:06:16 +0900

    update about.html / ss history

--- js/script.js
+++ js/script.js
@@ -10,12 +10,10 @@ $(function() {
 
   /* project */
   var pjList =  $('#project #sec2 ul li');
-  for (let i = 0; i < pjList.length; i++) {
-    var pjImg = $(pjList[i]).children('a').children('img').height();
+  for (var i = 0; i < pjList.length; i++) {
+    var height = $(pjList[i]).children('a').children('img').height();
 
-    $(pjList[i]).height(pjImg);
-    $(pjList[i]).children('a').height(pjImg);
-    $(pjList[i]).children('a').children('.pjBg').height(pjImg);
+    $(pjList[i]).height(height);
   }
 });
 
